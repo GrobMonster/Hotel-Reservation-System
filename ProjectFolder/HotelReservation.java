95a56276a661e4c8d7b4211614515ff0bad67233
GrobMonster <wyatt.brusseau@gmail.com>
2025-03-03 12:44:31 -0600

    Update HotelReservation.java

--- ProjectFolder/HotelReservation.java
+++ ProjectFolder/HotelReservation.java
@@ -1,1 +1,22 @@
+/* 
+Hotel Reservation System
+Created by: Cody Sandford & Wyatt Brusseau
 
+*/
+
+public class HotelReservation(String[] args){
+    class CustomerInformation{
+
+    }
+
+    class invoiceInformation{
+
+    }
+
+    class HotelReservationInformation{
+
+    }
+    class main(String[] args){
+
+    }
+}
