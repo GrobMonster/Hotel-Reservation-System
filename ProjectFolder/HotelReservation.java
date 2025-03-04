2680689a72129ec57fda0ec2a46a21b27300b92f
GrobMonster <wyatt.brusseau@gmail.com>
2025-03-04 11:53:57 -0600

    Create HotelReservation.java

---
+++ ProjectFolder/HotelReservation.java
@@ -0,0 +1,21 @@
+/*
+Main Program
+Created by Cody Sandford & Wyatt Brusseau
+This program will use the other classes in the
+package to give a central interface for the 
+hotel worker
+*/
+
+import java.util.Scanner;
+
+public class HotelReservation{
+    public static void main(String[] args){
+        int roomNumber;
+        int numOfBeds;
+        boolean isSuite;
+        boolean isRoomAvailable;
+        String customersRoom;
+
+      System.out.println("Hello! Welcome to the Hotel Reservation System!\n What would you like to start with?")
+      
+}
