498811d5b85291a08813592c926c43bca5992ca9
GrobMonster <wyatt.brusseau@gmail.com>
2025-03-04 12:52:45 -0600

    Update HotelReservation.java

--- ProjectFolder/HotelReservation.java
+++ ProjectFolder/HotelReservation.java
@@ -5,17 +5,62 @@ This program will use the other classes in the
 package to give a central interface for the 
 hotel worker
 */
-
 import java.util.Scanner;
 
 public class HotelReservation{
-    public static void main(String[] args){
+    void customerInformationTest(){
+        System.out.println("Ran CustomerInformationTest")
+    }
+
+    void roomInformationTest(){
         int roomNumber;
         int numOfBeds;
         boolean isSuite;
         boolean isRoomAvailable;
         String customersRoom;
+        Scanner keyboard = new keyboard.Scanner("System.in");
 
-      System.out.println("Hello! Welcome to the Hotel Reservation System!\n What would you like to start with?")
+        System.out.println("Ran roomInformationTest");
+    }
+    void invoiceInformationTest(){
+        System.out.println("Ran invoiceInformationTest")
+    }
+    void reservationInformationTest(){
+        System.out.println("Ran reservationInformationTest")
+    }
+
+    public static void main(String[] args){
+        Scanner keyboard = new keyboard.Scanner("System.in");
+
+        System.out.println("Hello! Welcome to the Hotel Reservation System!\n What would you like to start with? (Enter number that corresponds with you decision, enter 0 to quit)");
+        System.out.println("1.) Rooms");
+        System.out.println("2.) Customers"); 
+        System.out.println("3.) Invoice");
+        System.out.println("4.) Reservation");
+        int decision = keyboard.nextInt();
+
+        while (decision != 0){
+            if (decision == 1){
+                roomInformationTest();
+            }
+            if (decision == 2){
+                customerInformationTest();
+            }
+            if (decision = 3){
+                invoiceInformationTest();
+            }
+            if(decision = 4){
+                reservationInformationTest();
+            }
+            else{
+                System.out.println("Invalid number please enter a number given")
+            }
+            System.out.println("What would you like to do next? (Enter number that corresponds with you decision, enter 0 to quit)");
+            System.out.println("1.) Rooms");
+            System.out.println("2.) Customers"); 
+            System.out.println("3.) Invoice");
+            System.out.println("4.) Reservation
+            decision = keyboard.nextInt():
+            }
       
 }
