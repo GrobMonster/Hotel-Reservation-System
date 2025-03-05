f75a815d7f416d91528efa3cc7a235f3b69a5cc5
CodySand <codyjsand@gmail.com>
2025-03-04 21:59:07 -0600

    Update CustomerInformation.java

--- ProjectFolder/CustomerInformation.java
+++ ProjectFolder/CustomerInformation.java
@@ -1,1 +1,14 @@
+/*
+Created by Cody Sandford & Wyatt Brusseau
+CustomerInformation class for HotelReservation
+*/
+public class customerInformation{
+}
+
+String firstName;
+String lastName;
+String phoneNumber;
+String LicenceID;
+boolean isCheckedIn;
+
 
