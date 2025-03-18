86c15c0c625fcd1ef48bab8417d3bd007d0c9760
GrobMonster <wyatt.brusseau@gmail.com>
2025-03-18 18:40:17 -0500

    Update ReservationInformation.java

--- ProjectFolder/ReservationInformation.java
+++ ProjectFolder/ReservationInformation.java
@@ -4,22 +4,22 @@ Reservation Information
 Created by: Cody Sandford and Wyatt Brusseau
 
 */
-
+import java.util.ArrayList;
 public class ReservationInformation{
     private double pricePerNight;
     private int numOfNights;
-    ArrayList<int> customersRoomNumbers;
+    ArrayList<Integer> customersRoomNumbers;
     String customerName;
 
     //Constructors
     ReservationInformation(){
         this.pricePerNight = 0.0;
         this.numOfNights = 0;
-        this.customersRoomNumbers = null;
+        this.customersRoomNumbers = new ArrayList<>();
         this.customerName = "N/A";
     }
     
-    ReservationInformation(double pricePerNight, int numOfNights, ArrayList<int> customersRoomNumbers, String customerName){
+    ReservationInformation(double pricePerNight, int numOfNights, ArrayList<Integer> customersRoomNumbers, String customerName){
         this.pricePerNight = pricePerNight;
         this.numOfNights = numOfNights;
         this.customersRoomNumbers = customersRoomNumbers;
@@ -33,7 +33,7 @@ public class ReservationInformation{
     void setNumOfNights(int numOfNights){
         this.numOfNights = numOfNights;
     }
-    void setCustomersRoomNumbers(ArrayList<int> customersRoomNumbers){
+    void setCustomersRoomNumbers(ArrayList<Integer> customersRoomNumbers){
         this.customersRoomNumbers = customersRoomNumbers;
     }
     void setCustomerName(String customerName){
@@ -46,10 +46,14 @@ public class ReservationInformation{
     int getNumOfNights(){
         return this.numOfNights;
     }
-    ArrayList<int> getCustomersRoomNumbers(){
+    ArrayList<Integer> getCustomersRoomNumbers(){
         return this.customersRoomNumbers;
     }
     String getCustomerName(){
         return this.customerName;
+    }
+
+    public String toString(){
+        return "Reservation for: " + customerName + "\nRooms: " + customersRoomNumbers + "\nPrice per Night: $" + pricePerNight + "\nNumber of Nights: " + numOfNights;
     }
 }
