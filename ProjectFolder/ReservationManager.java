7eb1e7be7916ccedbad055ddb538749e3241edcb
GrobMonster <wyatt.brusseau@gmail.com>
2025-03-17 20:10:08 -0500

    Update ReservationManager.java

--- ProjectFolder/ReservationManager.java
+++ ProjectFolder/ReservationManager.java
@@ -3,3 +3,12 @@ Hotel Reservation System
 Reservation Manager
 Created By: Cody Sandford and Wyatt Brusseau
 */
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ReservationManager{
+    private ArrayList<ReservationInformation> listOfReservations;
+    private 
+    
+
+}
